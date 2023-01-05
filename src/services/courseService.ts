01ab37b747d511ff0f1a759221f79ffbb633896e
victorbtdias <victorbtdias159@hotmail.com>
2023-01-05 18:45:02 -0300

    Buscando por cursos

--- src/services/courseService.ts
+++ src/services/courseService.ts
@@ -1,4 +1,5 @@
 import { Course } from "../models";
+import { Op } from "sequelize";
 
 export const courseService = {
   findByIdWithEpisodes: async (id: string) => {
@@ -44,5 +45,27 @@ export const courseService = {
     });
 
     return courses;
+  },
+
+  findByName: async (name: string, page: number, perPage: number) => {
+    const offset = (page - 1) * perPage;
+
+    const { count, rows } = await Course.findAndCountAll({
+      attributes: ["id", "name", "synopsis", ["thumbnail_url", "thumbnailUrl"]],
+      where: {
+        name: {
+          [Op.iLike]: `%${name}%`,
+        },
+      },
+      limit: perPage,
+      offset: offset,
+    });
+
+    return {
+      courses: rows,
+      page,
+      perPage,
+      total: count,
+    };
   },
 };
