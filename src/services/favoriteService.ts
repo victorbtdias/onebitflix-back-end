24c46909baf59e6fc0d288b0044735e86cd70f75
victorbtdias <victorbtdias159@hotmail.com>
2023-01-07 18:12:07 -0300

    Atualizando o endpoint de cursos

--- src/services/favoriteService.ts
+++ src/services/favoriteService.ts
@@ -39,4 +39,15 @@ export const favoriteService = {
       },
     });
   },
+
+  isFavorited: async (userId: number, courseId: number) => {
+    const favorite = await Favorite.findOne({
+      where: {
+        userId,
+        courseId,
+      },
+    });
+
+    return favorite !== null ? true : false;
+  },
 };
