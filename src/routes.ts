455d763b2a6e822391d5e715e7631df0b44337f8
victorbtdias <victorbtdias159@hotmail.com>
2023-01-08 13:11:39 -0300

    Adicionando progresso do episódio

--- src/routes.ts
+++ src/routes.ts
@@ -22,6 +22,16 @@ router.get("/courses/search", ensureAuth, coursesController.search);
 router.get("/courses/:id", ensureAuth, coursesController.show);
 
 router.get("/episodes/stream", ensureAuthViaQuery, episodesController.stream);
+router.get(
+  "/episodes/:id/watchTime",
+  ensureAuth,
+  episodesController.getWatchTime
+);
+router.post(
+  "/episodes/:id/watchTime",
+  ensureAuth,
+  episodesController.setWatchTime
+);
 
 router.get("/favorites", ensureAuth, favoritesController.index);
 router.post("/favorites", ensureAuth, favoritesController.save);
